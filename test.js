eda16c6d14a2d3fb1729c433ec415bb5ff292c9c
Ajibola <ajibolabalogun.pius@gmail.com>
2025-08-19 00:36:47 +0100

    new commit

--- test.js
+++ test.js
@@ -1,26 +1,6 @@
-function createlogin(username , password){
-
-  var username = "admin"
-  var password ="1234"
-  if (username =="admin" && password =="1234"){
-    console.log("successful")
-  }else{
-    console.log("failed")
-  }
+var numbers = [1,3,4,4,5,9,10,6]
+var sum = 0
+for (var i = 0; i < numbers.length; i++){
+    sum = sum + numbers[i]
 }
-console.log(createlogin())
-
-function passwordstrenght(password){
-  var length = password.length
-  if( length < 6 ){
-    return "weak"
-  }else if(length <= 10){
-    return "moderate"
-  }
-  else{
-    return "strong"
-  }
-}
-console.log(passwordstrenght("aji"))
-console.log(passwordstrenght("ajibola123"))
-console.log(passwordstrenght("ajiolaadeola123"))
+console.log("sum: ", sum)
